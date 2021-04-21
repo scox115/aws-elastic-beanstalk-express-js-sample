8439cf9b445ee4566c9d0421f0af8f9d25890711
Scott Cox <scottlcox1961@gmail.com>
2021-04-20 21:55:38 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello to Scottie World!'));
+app.get('/', (req, res) => res.send('Push a new commit to your repo Scottie Lee Cox!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
